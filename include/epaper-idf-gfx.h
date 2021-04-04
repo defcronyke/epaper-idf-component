89cff686e09171bf92573b953a872bd92b37f9ba
Jeremy Carter <jeremy@jeremycarter.ca>
2021-04-03 23:58:46 -0400

    Fixes.

--- include/epaper-idf-gfx.h
+++ include/epaper-idf-gfx.h
@@ -23,24 +23,24 @@
 
 class EpaperIDFGFX : public virtual Adafruit_GFX
 {
-	public:
-		EpaperIDFGFX(int16_t w, int16_t h) 
-		:	Adafruit_GFX(w, h),
-			tag("epaper-idf")
-		{
-			ESP_LOGI(tag, "EpaperIDF %s constructed: EpaperIDF -> %s", EPAPER_IDF_VERSION, EPAPER_IDF_QUOTE(EPAPER_IDF_VERSION_CLASS));
-		};
+public:
+  EpaperIDFGFX(int16_t w, int16_t h)
+      : Adafruit_GFX(w, h),
+        tag("epaper-idf")
+  {
+    ESP_LOGI(tag, "%s constructed", EPAPER_IDF_QUOTE(EPAPER_IDF_VERSION_CLASS));
+  };
 
-		virtual ~EpaperIDFGFX()
-		{
-			ESP_LOGI(tag, "EpaperIDF %s deconstructed: EpaperIDF -> %s", EPAPER_IDF_VERSION, EPAPER_IDF_QUOTE(EPAPER_IDF_VERSION_CLASS));
-		};
+  virtual ~EpaperIDFGFX()
+  {
+    ESP_LOGI(tag, "%s deconstructed", EPAPER_IDF_QUOTE(EPAPER_IDF_VERSION_CLASS));
+  };
 
-		virtual void drawPixel(int16_t x, int16_t y, uint16_t colour) = 0;
-    virtual void init(uint32_t verbose = 0) = 0;
-    virtual void update() = 0;
+  virtual void drawPixel(int16_t x, int16_t y, uint16_t colour) = 0;
+  virtual void init(uint32_t verbose = 0) = 0;
+  virtual void update() = 0;
 
-	private:
-		const char *tag;
+private:
+  const char *tag;
 };
 #endif
