6bd066168472f63c7ebecdb339c57595770a69b0
Jeremy Carter <jeremy@jeremycarter.ca>
2021-04-05 18:18:21 -0400

    Update file header.

--- epaper-idf-http.c
+++ epaper-idf-http.c
@@ -7,6 +7,24 @@
 		this project. You must agree to follow those license terms,
 		otherwise you aren't allowed to copy, distribute, or use any 
 		part of this project in any way.
+
+		Contains some modified example code from here:
+		https://github.com/espressif/esp-idf/blob/release/v4.2/examples/protocols/https_mbedtls/main/https_mbedtls_example_main.c
+
+		Original Copyright (C) 2006-2016, ARM Limited, All Rights Reserved, Apache 2.0 License.
+		Additions Copyright (C) Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD, Apache 2.0 License.
+
+		Licensed under the Apache License, Version 2.0 (the "License");
+		you may not use this file except in compliance with the License.
+		You may obtain a copy of the License at
+
+			http://www.apache.org/licenses/LICENSE-2.0
+
+		Unless required by applicable law or agreed to in writing, software
+		distributed under the License is distributed on an "AS IS" BASIS,
+		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+		See the License for the specific language governing permissions and
+		limitations under the License.
 */
 #include <string.h>
 #include <stdlib.h>
@@ -249,19 +267,9 @@ void epaper_idf_http_task(void *pvParameter)
 
 			putchar('\n'); // JSON output doesn't have a newline at end
 
-			// static int request_count;
-
 			ESP_LOGI(TAG, "completed https request");
 
 			break;
-
-			// ESP_LOGI(TAG, "Completed %d requests", ++request_count);
-
-			// for(int countdown = 10; countdown >= 0; countdown--) {
-			//     ESP_LOGI(TAG, "%d...", countdown);
-			//     vTaskDelay(1000 / portTICK_PERIOD_MS);
-			// }
-			// ESP_LOGI(TAG, "Starting again!");
 		}
 
 		ESP_ERROR_CHECK(esp_event_post_to(epaper_idf_http_event_loop_handle, EPAPER_IDF_HTTP_EVENT, EPAPER_IDF_HTTP_EVENT_FINISH, NULL, 0, portMAX_DELAY));
