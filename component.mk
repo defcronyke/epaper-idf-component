cd8e94af7239bbc27e15ddf8b04415ed1ba6e6d4
Jeremy Carter <jeremy@jeremycarter.ca>
2021-04-06 22:06:07 -0400

    Add copy-certs.sh helper script.

--- component.mk
+++ component.mk
@@ -1,3 +1,13 @@
+# epaper-idf-component
+#
+# Copyright (c) 2021 Jeremy Carter <jeremy@jeremycarter.ca>
+#
+# This code is released under the license terms contained in the
+# file named LICENSE, which is found in the top-level folder in
+# this project. You must agree to follow those license terms,
+# otherwise you aren't allowed to copy, distribute, or use any 
+# part of this project in any way.
+
 COMPONENT_ADD_INCLUDEDIRS := include
 
 COMPONENT_EMBED_TXTFILES :=  ${PROJECT_PATH}/certs/ca_cert.pem
