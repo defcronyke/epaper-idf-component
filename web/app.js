43783f494663192d71865cc7bc53c5936c1821f7
Jeremy Carter <jeremy@jeremycarter.ca>
2021-04-21 20:25:13 -0400

    Improve config site.

--- web/app.js
+++ web/app.js
@@ -3,12 +3,10 @@ require('./favicon.png');
 require('./apple-touch-icon-precomposed.png');
 require('./world-soup-remastered-bw.png');
 
-// require('./NanumMyeongjo-Regular.ttf');
-
 require('./index.css');
 
-// require('./index.vendor.js');
-// require('./index.webpack.js');
 require('./index.js');
+
+require('./404.html');
 
 require('./index.html');
--- web/serve-prod.sh
+++ web/serve-prod.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 ./build.sh && \
-./node_modules/http-server/bin/http-server ../public -p 8080 -g -b --log-ip -r --no-dotfiles --proxy http://localhost:8080?
+./node_modules/http-server/bin/http-server ../public -p 8080 -g -b --log-ip -r --no-dotfiles
+
+# Use this line instead to bypass the 404.html page and serve index.html for not found errors.
+# ./node_modules/http-server/bin/http-server ../public -p 8080 -g -b --log-ip -r --no-dotfiles --proxy http://localhost:8080?
