59d5f7a7606f66870370cbe7ee43e69bab376266
Jeremy Carter <jeremy@jeremycarter.ca>
2021-04-22 18:17:02 -0400

    Config page restart button.

--- web/index.js
+++ web/index.js
@@ -1,7 +1,33 @@
 (function() {
 
 function restartButtonHandler() {
-    alert('restarting device...');
+    if (confirm('Would you like to restart the device?')) {
+        fetch('/api/util/restart', {
+            method: 'POST',
+        })
+        .then(function(res) {
+            if (res.status === 404) {
+                return { msg: 'DEMO MODE: no-op' };
+            }
+
+            return res.json();
+        })
+        .then(function(res) {
+            console.log('POST /api/util/restart response:');
+            console.log(res);
+
+            alert('The device is restarting...\n\n' + res.msg);
+
+            window.location.reload();
+
+            return res;
+        })
+        .catch(function(err) {
+            console.log('error: unexpected response from server:');
+            console.log(err);
+            return err;
+        });
+    }
 }
 
 function initButtons() {
